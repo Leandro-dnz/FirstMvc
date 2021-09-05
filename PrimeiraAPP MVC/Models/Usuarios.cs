51855623f70850b54c0806d3934a3ed0b292b054
Leandro <82003873+Leandro-dnz@users.noreply.github.com>
2021-09-05 19:44:02 -0300

    Adicionar arquivos de projeto.

---
+++ PrimeiraAPP MVC/Models/Usuarios.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PrimeiraAPP_MVC4.Models
+{
+    public class Usuarios
+    {
+        public List<UsuarioModel> listaUsuarios = new List<UsuarioModel>();
+        public Usuarios()
+        {
+            listaUsuarios.Add(new UsuarioModel
+            {
+                nome = "Leandro ",
+                sobrenome = " Diniz",
+                endereco = "Rua Projetada , 100",
+                email = "leandddro@outlook.com",
+                nascimento = Convert.ToDateTime("06/05/1992")
+            });
+            
+        }
+        public void CriaUsuario(UsuarioModel usuarioModelo)
+        {
+            listaUsuarios.Add(usuarioModelo);
+        }
+        public void AtualizaUsuario(UsuarioModel usuarioModelo)
+        {
+            foreach (UsuarioModel usuario in listaUsuarios)
+            {
+                if (usuario.email == usuarioModelo.email)
+                {
+                    listaUsuarios.Remove(usuario);
+                    listaUsuarios.Add(usuarioModelo);
+                    break;
+                }
+            }
+        }
+        public UsuarioModel GetUsuario(string Email)
+        {
+            UsuarioModel _usuarioModel = null;
+
+            foreach (UsuarioModel _usuario in listaUsuarios)
+                if (_usuario.email == Email)
+                    _usuarioModel = _usuario;
+
+            return _usuarioModel;
+        }
+    }
+}
